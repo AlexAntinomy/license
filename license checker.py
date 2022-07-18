9b728edcfe5d08d72ea40b350e4ce04cfb047aed
Antinomy <alexksa179@gmail.com>
2022-07-18 16:44:35 +0300

    func1

--- license checker.py
+++ license checker.py
@@ -1,2 +1,9 @@
 print ('Hello!')
 def check_status(age, license):
+    if int(age) > 18 and int(license) == 1:
+        print(' You are good to go, initiating systems')
+    elif int(age) < 18:
+        print('Hell no, your are still a kid!')
+    elif not license == 1:
+        print(' Get a license dude!, I will not run')
+    return check_status
